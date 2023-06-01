d7a7a5f8869dec33f72c120e1ffdc536d135f2b1
Wanderlee Max <wanderleedev@gmail.com>
2023-05-31 22:04:34 -0500

    atributo loading lazy agregado al componente card

--- assets/js/components/card.js
+++ assets/js/components/card.js
@@ -5,7 +5,7 @@ export const card = (data) => {
   const component = `
   <figure class="card">
     <div class="card-content">
-      <img class="card-img" src=${img} alt='foto de ${name}'>
+      <img loading="lazy" class="card-img" src=${img} alt='foto de ${name}'>
       <span class="card-extraData">${position}</span>
     </div>
     <figcaption class="card-title">${name}</figcaption>
